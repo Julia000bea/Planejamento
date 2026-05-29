2d1732e0a1509f663cd6b52e3b08a0a38293ea93
igoorique <igor.henrique@unochapeco.edu.br>
2026-05-29 20:39:23 -0300

    Delete igor_agendamed/agendamed directory

--- igor_agendamed/agendamed/src/middlewares/errorMiddleware.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-function errorMiddleware(err, req, res, next) {
-  console.error('[ERRO]', err.message);
-  res.status(500).sendFile(path.join(__dirname, '../views/erros/404.html'));
-}
-
-module.exports = errorMiddleware;
